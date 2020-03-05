5d173a9e574c15d60a878b87e0e51261777546ce
Xarizmo <mpolikarpoff@gmail.com>
2020-03-05 11:19:08 +0300

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://im0-tub-ru.yandex.net/i?id=5a2d2ff97ba9302c6e431f0c24556e98&n=13" alt=""/>
+    </header>
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div className='content-top-bg'>
+
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            myPosts
+        </div>
+        <div>
+            post1
+        </div>
+        <div>
+            post2
+        </div>
+
+    </div>
+}
+
+export default Profile;
