39968266371e7bcbd5201f194c4ac31a6267a368
Xarizmo <mpolikarpoff@gmail.com>
2020-03-15 00:09:17 +0300

    React-router-dom + new components (19)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import './App.css';
 
+import {BrowserRouter, Route} from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Nav from "./Components/Navbar/Navbar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
+import Music from "./Components/Music/Music";
+import News from "./Components/News/News";
+import Settings from "./Components/Settings/Settings";
 
 const App = () => {
   return (
-    <div className="app_wrapper">
-      <Header/>
-      <Nav/>
-      <div className="app_wrapper_content">
-        <Dialogs/>
+    <BrowserRouter>
+      <div className="app_wrapper">
+        <Header/>
+        <Nav/>
+        <div className="app_wrapper_content">
+          <Route path='/profile' component={Profile}/>
+          <Route path='/dialogs' component={Dialogs}/>
+          <Route path='/news' component={News}/>
+          <Route path='/music' component={Music}/>
+          <Route path='/settings' component={Settings}/>
+        </div>
       </div>
-      {/*<Profile/>*/}
-    </div>
+    </BrowserRouter>
   )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import s from './Navbar.module.css'
 
-const Nav = () => {
+const Nav = (props) => {
     return <nav className={s.nav}>
-        <div><a href="google.com">Profile</a></div>
-        <div><a href="google.com">Messages</a></div>
-        <div><a href="google.com">News</a></div>
-        <div><a href="google.com">Music</a></div>
-        <div><a href="google.com">Settings</a></div>
+        <div>
+            <a href="/profile">Profile</a>
+        </div>
+        <div>
+            <a href="/dialogs">Dialogs</a>
+        </div>
+        <div>
+            <a href="/news">News</a>
+        </div>
+        <div>
+            <a href="/music">Music</a>
+        </div>
+        <div>
+            <a href="/settings">Settings</a>
+        </div>
     </nav>
 }
 
