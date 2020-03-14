ef1426db429a5eb171f594d544712ed1e48b6ef1
Xarizmo <mpolikarpoff@gmail.com>
2020-03-14 11:34:17 +0300

    props (16)

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -10,11 +10,8 @@ const MyPosts = () => {
         New Post
       </div>
       <div className={s.posts}>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Post message='Hi, how are you?' likeCounts=' 15' />
+        <Post message={`It's my first post`} likeCounts=' 20' />
       </div>
     </div>
   );
--- src/Components/Profile/MyPosts/Post/Post.jsx
+++ src/Components/Profile/MyPosts/Post/Post.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className={s.item}>
       <img src="https://cdn.onlinewebfonts.com/svg/img_452489.png" alt=""/>
-      <span>post1</span>
-      <button>Like!</button>
+      <span>{ props.message }</span>
+      <button>Like!<span>{ props.likeCounts }</span></button>
     </div>
   );
 }
