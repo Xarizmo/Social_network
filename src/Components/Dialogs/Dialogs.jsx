b8feed33a6b377eda614e71843f5a55ff6e74282
Xarizmo <mpolikarpoff@gmail.com>
2020-03-15 20:38:55 +0300

    Dialog's page first steps (21)

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -3,8 +3,20 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-      <div className={s.content}>
-          Dialogs
+      <div className={s.dialogs}>
+        <div className={s.dialogs_items}>
+          <div className={s.dialog + ' ' + s.active}>Дима</div>
+          <div className={s.dialog}>Коля</div>
+          <div className={s.dialog}>Света</div>
+          <div className={s.dialog}>Маша</div>
+          <div className={s.dialog}>Петрович</div>
+          <div className={s.dialog}>Еще какой-то мужик</div>
+        </div>
+        <div className={s.messages}>
+          <div className={s.message}>Hi</div>
+          <div className={s.message}>Yo!</div>
+          <div className={s.message}>Hello!</div>
+        </div>
       </div>
     )
 }
