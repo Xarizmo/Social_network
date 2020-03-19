4602cae73bdc5b900a962a9c91de9559bd6af379
Xarizmo <mpolikarpoff@gmail.com>
2020-03-20 02:11:30 +0300

    Data migration to state.js (29)

---
+++ src/redux/state.js
@@ -0,0 +1,26 @@
+let state = {
+  dialogsPage : {
+    dialogs : [
+      {id: 1, name: 'Dima'},
+      {id: 2, name: 'Sveta'},
+      {id: 3, name: 'Marina'},
+      {id: 4, name: 'Mikhail'}
+    ],
+    messages : [
+      {id: 1, message: 'Hi!'},
+      {id: 2, message: 'Please call the police'},
+      {id: 3, message: 'I wonder where you are..'},
+      {id: 4, message: 'Push me! You cant just touch me!'}
+    ]
+  },
+  profilePage : {
+    posts : [
+      {id: 1, message: 'Hi, how are you?', likesCount: 12},
+      {id: 2, message: 'It\'s my first post', likesCount: 2},
+      {id: 3, message: 'It is impossible!', likesCount: 5},
+      {id: 4, message: 'Shut up and take my money!', likesCount: 122}
+    ]
+  }
+};
+
+export default state;
