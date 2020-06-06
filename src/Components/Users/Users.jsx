9b93d768ed68bcc112bb2c0d5ceeb6c3f17bc1de
Xarizmo <mpolikarpoff@gmail.com>
2020-06-07 02:48:05 +0300

    Class component implementation (50-53)

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,52 +1,56 @@
-import React from "react";
+import React, {Component} from "react";
 import s from './Users.module.css';
+import * as axios from 'axios';
 
-let Users = ({users, follow, unfollow, setUsers}) => {
-
-  if (users.length === 0) {
-    setUsers(
-      [
-        {id: 1, followed: true, fullName: 'Dennis Jr.', job: 'Senior React developer', location: {country: 'USA', city: 'NY'}},
-        {id: 2, followed: false, fullName: 'Margarita Ivanova', job: 'Senior React developer', location: {country: 'Russia', city: 'Moscow'}},
-        {id: 3, followed: true, fullName: 'Dmitry Kiselev', job: 'Senior React developer', location: {country: 'Belarus', city: 'Minsk'}},
-        {id: 4, followed: false, fullName: 'Ivan Dzyatko', job: 'Senior React developer', location: {country: 'Ukraine', city: 'Kiev'}}
-      ]
-    )
+export default class Users extends Component {
+  constructor(props) {
+    super(props);
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then(response => {
+        this.props.setUsers(response.data.items);
+      });
   }
   
-  return (
-    <div>
-      <div className={s.usersWrapper}>
-        {users.map(u => {
-          return (
-            <div className={s.user} key={u.id}>
-              <div className={s.userAvatarWrapper}>
-                <img className={s.userAvatar} src="images/user-avatar.png" alt=""/>
-                {
-                  u.followed
-                    ? <button className={s.followBtn} onClick={() => {unfollow(u.id)}}>Unfollow</button>
-                    : <button className={s.followBtn} onClick={() => {follow(u.id)}}>Follow</button>
-                }
-              </div>
-              <div className={s.userProfileWrapper}>
-                <div className={s.userProfileInfo}>
-                  <span className={s.userProfileName}>{u.fullName}</span>
-                  <span>
-                    <span>{u.location.city}, </span>
-                    <span>{u.location.country}</span>
+  render() {
+    return (
+      <div>
+        <div className={s.usersWrapper}>
+          {this.props.users.map(u => {
+            return (
+              <div className={s.user} key={u.id}>
+                <div className={s.userAvatarWrapper}>
+                  <img className={s.userAvatar}
+                       src={u.photos.small ? u.photos.small : "images/user-avatar.png"}
+                       alt=""/>
+                  {
+                    u.followed
+                      ? <button className={s.followBtn} onClick={() => {
+                        this.props.unfollow(u.id)
+                      }}>Unfollow</button>
+                      : <button className={s.followBtn} onClick={() => {
+                        this.props.follow(u.id)
+                      }}>Follow</button>
+                  }
+                </div>
+                <div className={s.userProfileWrapper}>
+                  <div className={s.userProfileInfo}>
+                    <span className={s.userProfileName}>{u.name}</span>
+                    <span>
+                    <span>{"u.location.city"}, </span>
+                    <span>{"u.location.country"}</span>
                   </span>
-                </div>
-                <div className={s.userProfileJob}>
-                  <span>{u.job}</span>
+                  </div>
+                  <div className={s.userProfileJob}>
+                    <span>{"u.job"}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })}
+        </div>
+        <button className={s.showBtn}>Show more</button>
       </div>
-      <button className={s.showBtn}>Show more</button>
-    </div>
-  );
+    );
+  }
 }
-
-export default Users;
